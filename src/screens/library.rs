222a8da9f808faebf3f0bab8a930e545c82d527a
joegoggin <joegoggin27@gmail.com>
2025-02-23 02:02:59 -0500

    Add Home Screen

---
+++ src/screens/library.rs
@@ -0,0 +1,70 @@
+use log::debug;
+use ratatui::{crossterm::event::KeyEvent, style::Color, Frame};
+
+use crate::{
+    components::{screen_block::ScreenBlock, Component},
+    core::{app::App, config::Config, spotify::SpotifyClient},
+    AppResult, Message,
+};
+
+use super::{
+    auth::{create_config::CreateConfigFormScreen, show_link::ShowAuthLinkScreen},
+    Screen, ScreenType,
+};
+
+#[derive(Clone)]
+pub struct LibraryScreen;
+
+impl Default for LibraryScreen {
+    fn default() -> Self {
+        Self
+    }
+}
+
+impl Screen for LibraryScreen {
+    fn get_screen_type(&self) -> ScreenType {
+        ScreenType::LibraryScreen
+    }
+}
+
+impl Component for LibraryScreen {
+    fn view(&mut self, app: &App, frame: &mut Frame) {
+        ScreenBlock::new_with_color("Library", Color::Green).view(app, frame);
+    }
+
+    fn tick(&mut self, app: &mut App) -> AppResult<Option<Message>> {
+        match app.spotify_client.clone() {
+            Some(spotify_client) => {
+                if spotify_client.credentials.is_none() {
+                    let new_screen = Box::new(ShowAuthLinkScreen::new(spotify_client.auth_url));
+
+                    return Ok(Some(Message::ChangeScreen { new_screen }));
+                }
+
+                Ok(None)
+            }
+            None => {
+                let config = Config::new()?;
+                let result = SpotifyClient::new(config.clone());
+
+                match result {
+                    Ok(spotify_client) => {
+                        app.spotify_client = Some(spotify_client);
+
+                        Ok(None)
+                    }
+                    Err(_) => {
+                        let new_screen = Box::new(CreateConfigFormScreen::new(&config));
+
+                        Ok(Some(Message::ChangeScreen { new_screen }))
+                    }
+                }
+            }
+        }
+    }
+
+    fn handle_key_press(&mut self, _: &mut App, key: KeyEvent) -> AppResult<Option<Message>> {
+        debug!("{:#?}", key);
+        Ok(None)
+    }
+}
--- src/screens/mod.rs
+++ src/screens/mod.rs
@@ -1,6 +1,12 @@
 pub mod auth;
 pub mod exit;
 pub mod home;
+pub mod library;
+pub mod now_playing;
+pub mod queue;
 pub mod screen;
+pub mod search;
+pub mod view_album;
+pub mod view_artist;
 
 pub use screen::{Screen, ScreenType};
