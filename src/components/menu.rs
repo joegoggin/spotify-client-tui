66e4002adfb2712db623155287d38692d69545d1
joegoggin <joegoggindev@gmail.com>
2025-03-31 21:00:03 -0400

    Fix Menu To Handle More Than Just 6 Menu Items (Add Pagination)

--- src/components/menu.rs
+++ src/components/menu.rs
@@ -1,5 +1,6 @@
 use std::usize;
 
+use log::debug;
 use ratatui::{
     crossterm::event::{KeyCode, KeyEvent},
     layout::{Constraint, Layout, Rect},
@@ -18,6 +19,7 @@ pub struct Menu {
     pub current_menu_index: usize,
     pub menu_items: Vec<String>,
     pub area: Option<Rect>,
+    pub current_page: usize,
 }
 
 impl Default for Menu {
@@ -27,6 +29,7 @@ impl Default for Menu {
             current_menu_index: 0,
             menu_items: Vec::<String>::new(),
             area: None,
+            current_page: 1,
         }
     }
 }
@@ -38,6 +41,7 @@ impl Menu {
             current_menu_index: 0,
             menu_items,
             area: None,
+            current_page: 1,
         }
     }
 
@@ -47,10 +51,38 @@ impl Menu {
 
     fn get_item_color(&self, index: usize) -> Color {
         if self.current_menu_index == index {
-            return Color::Blue;
+            return Color::Green;
         }
 
         Color::White
+    }
+
+    fn get_total_pages(&self) -> usize {
+        let mut total_pages = self.menu_items.len() / 6;
+
+        if self.menu_items.len() % 6 != 0 {
+            total_pages += 1;
+        }
+
+        total_pages
+    }
+
+    fn get_start_index(&self) -> usize {
+        if self.current_page == 1 {
+            return 0;
+        }
+
+        (self.current_page - 1) * 6
+    }
+
+    fn get_end_index(&self) -> usize {
+        let mut end_index = self.get_start_index() + 6;
+
+        if end_index > self.menu_items.len() {
+            end_index = self.menu_items.len();
+        }
+
+        end_index
     }
 }
 
@@ -65,10 +97,11 @@ impl Component for Menu {
                 area = menu_area;
             }
 
-            constraints.push(Constraint::Percentage(10));
+            for _ in 0..6 {
+                constraints.push(Constraint::Max(3));
+            }
 
             for (index, item) in self.menu_items.iter().enumerate() {
-                constraints.push(Constraint::Max(3));
                 let color = self.get_item_color(index);
                 let title = item.to_string();
 
@@ -81,16 +114,37 @@ impl Component for Menu {
                 items.push(paragraph);
             }
 
+            for _ in 0..3 {
+                constraints.push(Constraint::Min(1));
+            }
+
             let menu_chunks = Layout::default()
-                .margin(5)
+                .margin(4)
                 .constraints(constraints)
                 .split(area);
 
-            for i in 0..self.menu_items.len() {
-                let paragraph = items[i].clone();
+            let start_index: usize = self.get_start_index().into();
+            let end_index: usize = self.get_end_index().into();
 
-                frame.render_widget(paragraph, menu_chunks[i + 1])
+            debug!("start_index: {}", start_index);
+            debug!("end_index: {}", end_index);
+
+            for i in start_index..end_index {
+                let paragraph = items[i].clone();
+                let mut menu_chunks_index = i;
+
+                if self.current_page > 1 {
+                    menu_chunks_index = i - ((self.current_page - 1) * 6);
+                }
+
+                frame.render_widget(paragraph, menu_chunks[menu_chunks_index]);
             }
+
+            let page_count_string =
+                format!("Page {} of {}", self.current_page, self.get_total_pages());
+            let paragraph = Paragraph::new(page_count_string).centered();
+
+            frame.render_widget(paragraph, menu_chunks[7]);
         }
     }
 
@@ -101,6 +155,14 @@ impl Component for Menu {
     fn handle_key_press(&mut self, _: &mut App, key: KeyEvent) -> AppResult<Option<Message>> {
         match key.code {
             KeyCode::Char('j') => {
+                if self.current_menu_index >= self.get_end_index() - 1 {
+                    if self.current_page + 1 <= self.get_total_pages() {
+                        self.current_page = self.current_page + 1;
+                    } else {
+                        self.current_page = 1;
+                    }
+                }
+
                 if self.current_menu_index < self.menu_items.len() - 1 {
                     self.current_menu_index = self.current_menu_index + 1;
                 } else {
@@ -109,6 +171,14 @@ impl Component for Menu {
                 Ok(None)
             }
             KeyCode::Char('k') => {
+                if self.current_menu_index == self.get_start_index() {
+                    if self.current_page == 1 {
+                        self.current_page = self.get_total_pages();
+                    } else {
+                        self.current_page = self.current_page - 1;
+                    }
+                }
+
                 if self.current_menu_index > 0 {
                     self.current_menu_index = self.current_menu_index - 1;
                 } else {
